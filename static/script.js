b4f34c92a1e9f08b6e607b588eb657bd4ca33a24
NoahIsGreatest <makeistrue@gmail.com>
2025-08-24 07:28:19 +0300

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,11 +1,18 @@
-$(document).ready(function(){
-    $("#watchAd").click(function(){
-        $.post("/watch_ad", {username: "{{ username }}"}, function(data){
-            if(data.balance !== undefined){
-                $("#balance").text(data.balance);
-                alert("+0.5 credits added!");
-            } else {
-                alert(data.error);
+$(document).ready(function() {
+    $("#watchAdBtn").click(function() {
+        if(!USERNAME) return alert("User not found");
+        $.ajax({
+            type: "POST",
+            url: "/watch_ad",
+            contentType: "application/json",
+            data: JSON.stringify({username: USERNAME}),
+            success: function(data) {
+                if(data.balance !== undefined){
+                    $("#balance").text(data.balance);
+                    alert("+0.5 credits added!");
+                } else {
+                    alert(data.error);
+                }
             }
         });
     });
@@ -15,14 +22,21 @@ $(document).ready(function(){
     });
 
     $("#confirmWithdraw").click(function(){
-        var amount = $("#withdrawAmount").val();
-        $.post("/withdraw", {username: "{{ username }}", amount: amount}, function(data){
-            if(data.success){
-                alert(data.success);
-                $("#balance").text(data.balance);
-                $("#withdrawForm").hide();
-            } else {
-                alert(data.error);
+        const amount = $("#withdrawAmount").val();
+        if(!USERNAME) return alert("User not found");
+        $.ajax({
+            type: "POST",
+            url: "/withdraw",
+            contentType: "application/json",
+            data: JSON.stringify({username: USERNAME, amount: amount}),
+            success: function(data){
+                if(data.success){
+                    alert(data.success);
+                    $("#balance").text(data.balance);
+                    $("#withdrawForm").hide();
+                } else {
+                    alert(data.error);
+                }
             }
         });
     });
